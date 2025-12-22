730083dcfbb0449f9b4247c3d00599f55820d3e7
Aman Sharma <115157700+the-amansharma@users.noreply.github.com>
2025-12-22 22:10:38 +0530

    Update main_api.py

--- main_api.py
+++ main_api.py
@@ -96,4 +96,5 @@ def search_notifications(payload: SearchRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
